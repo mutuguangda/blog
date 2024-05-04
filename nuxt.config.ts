717f69d0b02e76b813d57e9ef2a1f082effc78cc
mutuguangda <mutuguangda@foxmail.com>
2024-05-04 16:39:04 +0800

    feat: optimize markdown style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  content: {
+    highlight: {
+      theme: 'vitesse-light',
+    }
+  },
+  
   modules: ['@nuxt/content', '@unocss/nuxt']
 })
